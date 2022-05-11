3f8f8439fdd170ec19b9631713e377242a35858c
Flyeeer <105268847+Flyeeer@users.noreply.github.com>
2022-05-11 11:53:51 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("这里是master修改的");
         System.out.println("这里是hotfix再次修改的");
         System.out.println("idea里面Push的");
+        System.out.println("我在github修改了，请pull");
     }
 }
