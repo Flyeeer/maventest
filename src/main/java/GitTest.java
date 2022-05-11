5fff6e62d155a6ec22d4a059a19510bb41dc67a8
Fly <fly1091130646@qq.com>
2022-05-11 11:06:45 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("first");
         System.out.println("first2");
+        System.out.println("first2");
     }
 }
