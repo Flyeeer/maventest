341e283e4887fb30275ae6be8f4d705ef5667d6a
Fly <fly1091130646@qq.com>
2022-05-11 11:00:27 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * @author Fly
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first");
+    }
+}
